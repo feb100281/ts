41920d6445d4ad49a644d0a605bc5945ade2b0dd
feb100281 <feb100281@gmail.com>
2026-03-12 00:57:20 +0600

    wip

--- utils/accurals/cashbased.py
+++ utils/accurals/cashbased.py
@@ -2,11 +2,27 @@
 #--------------------------
 # Начисления CashBased
 #--------------------------
-import json
 import pandas as pd
 import numpy as np
 
-# Берет все условия с cashbased и распределяет по GL за 1 проход.
-def cash_based_distibution(**kwarg):
-    pass
+# распределяет по GL за 1 проход сf по счетам.
+def cash_based_distibution(conn,**args):
+    
+    param = args['params_json']
+    
+    contract_id = args['contract_id']
+    SQL = """
+    SELECT 
+    
+    """
+    
+    
+    return param['Игнорировать']
 
+
+def main(conn, **args):    
+    print(cash_based_distibution(conn, **args))
+    
+
+if __name__ == "__main__":
+    main()
