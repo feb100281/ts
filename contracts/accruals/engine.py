25e40060315d162d43fb3406e8863b0918413702
DariaVoitenko <daria031288d@gmail.com>
2026-03-11 13:55:12 +1000

    2026-03-11 1355

--- contracts/accruals/engine.py
+++ contracts/accruals/engine.py
@@ -1,6 +1,7 @@
 # contracts/accruals/engine.py
 from .registry import ACCRUAL_REGISTRY
 from .calculators.fixed_payments import preview as preview_fixed_payments
+from .calculators.fixed_total_by_period import preview as preview_fixed_total_by_period
 from .calculators.by_bank_statement import preview as preview_by_bank_statement
 from .calculators.rent_premises import preview as preview_rent_premises
 from .calculators.deposit_by_bank_statement import preview as preview_deposit_by_bank_statement
@@ -11,6 +12,7 @@ from .calculators.annual_payment import preview as preview_annual_payment
 
 PREVIEW_HANDLERS = {
     "fixed_payments": preview_fixed_payments,
+    "fixed_total_by_period": preview_fixed_total_by_period,
     "by_bank_statement": preview_by_bank_statement,
     "rent_premises": preview_rent_premises,
     "deposit_by_bank_statement": preview_deposit_by_bank_statement,
