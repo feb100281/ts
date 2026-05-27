1c776452de9e65ed55fd7b4d84ac6169dc5910cc
feb100281 <feb100281@gmail.com>
2026-05-27 19:44:31 +0600

    mfr

--- reports/app/slides/upd/data.py
+++ reports/app/slides/upd/data.py
@@ -1,2 +1,14 @@
 from conns import get_duckdb_conn_with_opt
 
+from ...queries import BaseQueries
+
+bq = BaseQueries()
+
+def get_upd_data():
+      
+    
+    return bq.upd_documents()
+    
+         
+         
+         
---
+++ reports/app/slides/upd/write_off.py
@@ -0,0 +1,90 @@
+import dash_mantine_components as dmc
+from dash_iconify import DashIconify
+from cards.wo_app.data import get_data_by_item
+from ...misc import(
+    paper_card,
+    fancy_numbers
+)
+import pandas as pd
+import locale
+
+locale.setlocale(
+    locale.LC_TIME,
+    "ru_RU.UTF-8"
+)
+
+
+""" 
+Слайд со списаниями й по отработанным УПД
+"""
+
+df_list = {
+2024:get_data_by_item('2024-01-01','2024-12-31'),
+2025:get_data_by_item('2025-01-01','2025-12-31'),
+2026:get_data_by_item('2026-01-01','2026-12-31'),
+}
+
+def make_revenue_comparison(year,df:pd.DataFrame):
+    revenue = df['amount_vatless'].sum()
+    comparison_revenue = df['comparison_revenue'].sum()
+    diff = revenue - comparison_revenue
+    total = revenue+comparison_revenue+diff
+    return dmc.Group(
+        [
+            dmc.Title(str(year),order=4),
+            dmc.ProgressRoot(
+                [
+                dmc.ProgressSection(
+                    dmc.ProgressLabel(f"Выручка {revenue/1_000_000:,.0f}M"),
+                    value = round(revenue/total*100,0),
+                    color="cyan"
+                ),
+                dmc.ProgressSection(
+                    dmc.ProgressLabel(f"Алокировано с/с {comparison_revenue/1_000_000:,.0f}M"),
+                    value = round(comparison_revenue/total*100,0),
+                    color="pink"
+                ),
+                dmc.ProgressSection(
+                    dmc.ProgressLabel(f"Нет с/с {diff/1_000_000:,.0f}M"),
+                    value = round(diff/total*100,0),
+                    color="orange"
+                ),     
+                ],
+                size="30",
+                style={
+                "flex": 1,
+                },                
+            )
+        ],
+        align="center",
+        style={
+        "width": "100%",
+        },
+    )
+
+revenue_card_list = []
+for k,v in  df_list.items():
+    revenue_card_list.append(make_revenue_comparison(k,v))
+
+process_title = dmc.Group(
+    [
+        DashIconify(icon='streamline-ultimate-color:cash-payment-bills-1',width=26),
+        dmc.Title("Списания по выручки",order=3)
+    ]
+)
+
+revenue_card = paper_card(
+    process_title,
+    dmc.Stack(
+        children=revenue_card_list,
+        gap=2
+    )
+)
+
+def layout(report=None, filters=None):
+    return dmc.Container(
+        [
+            revenue_card
+        ],
+        fluid=True
+    )
