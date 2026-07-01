965075d5e5a7bb68e73cbe6857984f40e5a6ec65
feb100281 <feb100281@gmail.com>
2026-07-01 10:11:24 +0300

    tree ?

--- gear/apps.py
+++ gear/apps.py
@@ -4,3 +4,10 @@ from django.apps import AppConfig
 class GearConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "gear"
+    verbose_name = "Dash Board"
+    
+    
+    def ready(self):       
+            # Импортируем Dash-приложение при старте Django
+            from .app.segement_sales import app as segments_sales_app
+            
