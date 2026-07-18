e462468eb22370e37e9063d1707d320847f58c64
DariaVoitenko <daria031288d@gmail.com>
2026-07-18 13:55:23 +1000

    2026-07-18 1335

---
+++ gear/app/costs_control/article_report/__init__.py
@@ -0,0 +1,11 @@
+from .callbacks import register_article_report_callbacks
+from .layout import (
+    build_article_report_button,
+    build_article_report_components,
+)
+
+__all__ = [
+    "build_article_report_button",
+    "build_article_report_components",
+    "register_article_report_callbacks",
+]
